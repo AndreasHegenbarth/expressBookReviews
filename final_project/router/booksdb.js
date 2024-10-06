eec555d0996f696637792bb315bc43ea30b3188d
Andreas Hegenbarth <andreas.hegenbarth@beyerdynamic.de>
2024-10-06 22:35:37 +0200

    Task 5

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,13 +1,13 @@
 let books = [
-      {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} , "isbn": "1234" },
+      {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": { "isbn": "1334"} , "isbn": "1234" },
        {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "isbn": "1334"},
       {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "isbn": "1233"},
       {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "isbn": "1134"},
        {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "isbn": "1214"},
        {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "isbn": "1231" },
-      {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "isbn": "1434" },
+      {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": { "isbn": "1334"}, "isbn": "1434" },
        {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "isbn": "1244" },
-       {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "isbn": "1114" },
+       {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": { "isbn": "1334"}, "isbn": "1114" },
        {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "isbn": "1111" }
 ]
 
