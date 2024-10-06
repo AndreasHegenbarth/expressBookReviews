2e23f732a0f13077484cfb3572ff2d5379ef2b43
Andreas Hegenbarth <andreas.hegenbarth@beyerdynamic.de>
2024-10-06 22:28:04 +0200

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,7 +27,9 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filtered_author = books.filter((book) => book.author === author);
+  res.send(filtered_author);
 });
 
 // Get all books based on title
